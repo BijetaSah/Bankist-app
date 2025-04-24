2fdec1a7cdfff41bd23a24feff07f9e57fe41b8f
BijetaSah <bijetasah@30gmail.com>
2025-04-24 22:26:00 +0530

    Added functionaslity of displaying balance and the balance summary

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Bijeta Sah',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Dhriti Dewan',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Priya Mathur',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Adhrit Chauhan',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -74,3 +74,55 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+containerApp.style.opacity = '1';
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">Rs.${mov}</div>
+        </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `Rs.${balance}`;
+};
+calcDisplayBalance(movements);
+
+const computingUserName = function (accounts) {
+  accounts.forEach(acc => {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+computingUserName(accounts);
+
+const calcDisplaySummary = function (movements) {
+  const deposit = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, cur) => acc + cur);
+  const withdrawal = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, cur) => acc + cur);
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(mov => mov * (1.2 / 100))
+    .filter(mov => mov >= 1)
+    .reduce((acc, cur) => acc + cur);
+  console.log(deposit, withdrawal, interest);
+  labelSumIn.textContent = `Rs.${deposit}`;
+  labelSumOut.textContent = `Rs.${withdrawal}`;
+};
+
+calcDisplaySummary(movements);
