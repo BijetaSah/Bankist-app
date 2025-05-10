1af83c5d57ba9a365d839af2e76da3e306d150d5
BijetaSah <bijetasah@30gmail.com>
2025-05-10 22:14:02 +0530

    Making the page responsive

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const formatMovementsDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
   const daysPassed = calcDaysPassed(new Date(), date);
-  console.log(new Date(daysPassed));
+
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago `;
